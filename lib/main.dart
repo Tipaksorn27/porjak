a1d4f72e5b9f7ba435e2fb0b659affbd8c2b6371
Tipaksorn27 <Tipaksorn27@gmail.com>
2022-08-28 11:41:19 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:porjak/pages/home.dart';
+
+
+void main() => runApp (Myapp());
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:  "Food Recipes",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(color: Colors.white),
+        ),
+      ),
+      home: const HomePage(),
+    );
+    
+  }
+}
